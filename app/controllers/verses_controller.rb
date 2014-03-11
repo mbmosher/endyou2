de00e1af1ce08cd1b634473cbf8d1247ff497ff2
mbmosher <mbmosher@gmail.com>
2014-03-10 18:14:26 -0700

    add new page, fix unlock function, add aesthetics.

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,21 +1,36 @@
 class VersesController < ApplicationController
+   http_basic_authenticate_with name: "mbmosher", password: "46k2ll", except: [:index, :unlock, :checkcode]
 
 def new
 	@verse = Verse.new
 end
 
 def index
-	@showUnlock = false
-	@verses = Verse.all
+	@showUnlocked = false
+	@verses = Verse.all.order(:id)
 	@verses.each do |verse|
 		if verse.unlocked == false
-			verse.text = 'xxxxx'
+			verse.text = '
+			Xxxx xxxx 
+			Xxxxxx xx xxxxxxxx
+			Xxx xxxxxxxx xx xx xxxxxx xxx
+			Xxxxxxxxxx xxxxxx
+			'
 			@showUnlocked = true
 		end
 	end
 end
 
 def create
+  @verse = Verse.new(verse_params)
+  @verse.unlocked = false
+  @verse.save
+
+	redirect_to verses_path
+end
+
+
+def unlock
 	testverse = Verse.new(params[:verse].permit(:code))
 	@verse = Verse.find_by code: testverse.code
 	if @verse
@@ -27,6 +42,24 @@ def create
 		# render 'unlock'
 	end
 	redirect_to verses_path
+end
+
+def checkcode
+  testverse = Verse.new(params[:verse].permit(:code))
+	@verse = Verse.find_by code: testverse.code
+	if @verse
+		@verse.unlocked = true
+		@verse.save
+		# redirect_to verses_path
+	else
+		flash[:notice] = "Invalid code"
+		# render 'unlock'
+	end
+  redirect_to verses_path
+end
+
+def verse_params
+	params.require(:verse).permit(:text, :code, :image, :map)
 end
 
 =begin
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131028000050) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "verses", force: true do |t|
     t.string   "image"
     t.string   "map"
